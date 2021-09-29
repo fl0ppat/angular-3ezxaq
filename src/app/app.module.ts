7fb4ec1ccbbd352f0b5dbf5825ada5387d6f3283
fl0ppat <klaven011@gmail.com>
2021-09-29 10:55:01 +0300

    feat: add cart and shipping prices

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,21 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CommonModule } from '@angular/common';
+import { CartComponent } from './cart/cart.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
   imports: [
+    CommonModule,
     BrowserModule,
+    HttpClientModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
+      { path: 'cart', component: CartComponent },
+      { path: 'shipping', component: ShippingComponent },
     ]),
   ],
   declarations: [
@@ -24,6 +31,9 @@ import { CommonModule } from '@angular/common';
     TopBarComponent,
     ProductListComponent,
     ProductAlertsComponent,
+    ProductDetailsComponent,
+    CartComponent,
+    ShippingComponent,
   ],
   bootstrap: [AppComponent],
 })
